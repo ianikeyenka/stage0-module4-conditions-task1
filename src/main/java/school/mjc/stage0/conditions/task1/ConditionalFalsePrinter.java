f07be11bfe76f30411196c7ed8cb4e9392d52078
Ilya Anikeyenka <20prorok02@gmail.com>
2023-02-13 20:43:23 +0300

    Solution

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean value = false;
+        if (!value) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean value = true;
+        if (value) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
